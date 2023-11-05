25466a26caf3b0273a44344cd836bbfad2a30012
Sonali Rana <ranasonali987@gmail.com>
2023-11-05 18:19:19 +0530

    final project

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
           value={firstName}
           placeholder="First Name"
           onChange={(event) => setFirstName(event.target.value)}
+          disabled={iseRegistered}
         />
         {isError && !firstName ? (
           <div className="warning">*Please Enter First Name</div>
@@ -36,6 +37,7 @@ function App() {
           value={lastName}
           placeholder="Last Name"
           onChange={(event) => setLastName(event.target.value)}
+          disabled={iseRegistered}
         />
         {isError && !lastName ? (
           <div className="warning">*Please Enter Last Name</div>
@@ -44,11 +46,12 @@ function App() {
           value={email}
           placeholder="Email"
           onChange={(event) => setEmail(event.target.value)}
+          disabled={iseRegistered}
         />
         {isError && !email ? (
           <div className="warning">*Please Enter Email</div>
         ) : null}
-        <button type="submit" className="submit-button">
+        <button type="submit" className="submit-button" disabled={iseRegistered}>
           Register
         </button>
       </form>
